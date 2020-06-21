3d6e11bc34d330b73621462226d6708310c72e21
Dmitry Kisler <admin@dkisler.com>
2020-06-21 20:47:25 +0200

    adds sql params parser

--- sql-runner-redshift/helper/helper.go
+++ sql-runner-redshift/helper/helper.go
@@ -1,5 +1,9 @@
 package helper
 
+import (
+	"encoding/json"
+)
+
 // InArrayStr function to check if a string element is present in an array of string
 func InArrayStr(array []string, testElement string) bool {
 	for _, s := range array {
@@ -8,4 +12,14 @@ func InArrayStr(array []string, testElement string) bool {
 		}
 	}
 	return false
+}
+
+// SQLParametersParser function to unmarshal SQL formatting parameters
+func SQLParametersParser(input string) (map[string]interface{}, error) {
+	var output map[string]interface{}
+	err := json.Unmarshal([]byte(input), &output)
+	if err != nil {
+		return nil, err
+	}
+	return output, nil
 }
--- sql-runner-redshift/helper/helper_test.go
+++ sql-runner-redshift/helper/helper_test.go
@@ -1,6 +1,8 @@
 package helper_test
 
 import (
+	"fmt"
+	"reflect"
 	"testing"
 
 	"github.com/kislerdm/sql-runner/sql-runner-redshift/helper"
@@ -28,6 +30,47 @@ func TestInArrayStr(t *testing.T) {
 		got := helper.InArrayStr(tt.array, tt.testElement)
 		if tt.want != got {
 			t.Fatalf("Results don't match\ngot: %v\nwant: %v", got, tt.want)
+		}
+	}
+}
+
+func TestSQLParametersParser(t *testing.T) {
+	tests := []struct {
+		in   string
+		want map[string]interface{}
+		err  error
+	}{
+		{
+			in: `{"a": 1, "b": "col1"}`,
+			want: map[string]interface{}{
+				"a": 1.,
+				"b": "col1",
+			},
+			err: nil,
+		},
+		{
+			in:   `{"a": 1, "b": "col1"`,
+			want: nil,
+			err:  fmt.Errorf("unexpected end of JSON input"),
+		},
+	}
+
+	for _, tt := range tests {
+		got, gotErr := helper.SQLParametersParser(tt.in)
+
+		if !reflect.DeepEqual(tt.want, got) {
+			t.Fatalf("Result outputs don't match\ngot: %v\nwant: %v", got, tt.want)
+		}
+
+		switch tt.err == nil {
+		case true:
+			if gotErr != nil {
+				t.Fatalf("Result errors don't match\ngot: %v\nwant: %v", gotErr, tt.err)
+			}
+		case false:
+			if gotErr.Error() != tt.err.Error() {
+				t.Fatalf("Result errors don't match\ngot: %v\nwant: %v", gotErr, tt.err)
+			}
 		}
 	}
 }
