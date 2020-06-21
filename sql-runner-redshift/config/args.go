16ed85fa02520390e693405e84837ee33ce463fe
Dmitry Kisler <admin@dkisler.com>
2020-06-21 19:15:25 +0200

    refactors

--- sql-runner-redshift/config/args.go
+++ sql-runner-redshift/config/args.go
@@ -1,4 +1,4 @@
-package inputconfig
+package config
 
 import (
 	"flag"
---
+++ sql-runner-redshift/config/env.go
@@ -0,0 +1,79 @@
+package config
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+
+	"github.com/kislerdm/sql-runner/sql-runner-redshift/connectors/redshift"
+	"github.com/kislerdm/sql-runner/sql-runner-redshift/connectors/s3"
+)
+
+// EnvVars env vars configuration
+type EnvVars struct {
+	AWS *s3.Config
+	DB  *redshift.Config
+}
+
+// ReadEnvVars function to read env variables
+func ReadEnvVars() (*EnvVars, error) {
+	// AwsAccessKeyID := os.Getenv("AWS_ACCESS_KEY_ID")
+	// AwsSecretAccessKey := os.Getenv("AWS_SECRET_ACCESS_KEY")
+	AwsRegion := os.Getenv("AWS_DEFAULT_REGION")
+
+	// if !(AwsAccessKeyID != "" && AwsSecretAccessKey != "") {
+	// 	return &EnvVars{},
+	// 		fmt.Errorf("export 'AWS_ACCESS_KEY_ID' and 'AWS_SECRET_ACCESS_KEY' as env vars")
+	// }
+
+	if AwsRegion == "" {
+		AwsRegion = "us-west-2"
+	}
+
+	Host := os.Getenv("DB_HOST")
+	if Host == "" {
+		Host = "localhost"
+	}
+
+	Port := os.Getenv("DB_PORT")
+	var PortInt int
+	if Port == "" {
+		PortInt = 5432
+	} else {
+		p, err := strconv.Atoi(Port)
+		if err != nil {
+			return &EnvVars{}, fmt.Errorf("specify int port as 'DB_PORT' env variable")
+		}
+		PortInt = p
+	}
+
+	DbName := os.Getenv("DB_NAME")
+	if DbName == "" {
+		DbName = "postgres"
+	}
+
+	User := os.Getenv("DB_USER")
+	if User == "" {
+		User = "postgres"
+	}
+
+	DbPassword := os.Getenv("DB_PASSWORD")
+	if DbPassword == "" {
+		DbPassword = "postgres"
+	}
+
+	return &EnvVars{
+		AWS: &s3.Config{
+			// AwsAccessKeyID:     AwsAccessKeyID,
+			// AwsSecretAccessKey: AwsSecretAccessKey,
+			Region: AwsRegion,
+		},
+		DB: &redshift.Config{
+			Host:       Host,
+			Port:       PortInt,
+			DbName:     DbName,
+			User:       User,
+			DbPassword: DbPassword,
+		},
+	}, nil
+}
