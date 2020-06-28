6537670ff9a07ca20d28b9d8d38121624e040021
Dmitry Kisler <dmitry.kisler@deliveryhero.com>
2020-06-29 01:30:46 +0200

    fixes strip-n-split methods

--- sql-runner-redshift/sqlparser/sqlparser.go
+++ sql-runner-redshift/sqlparser/sqlparser.go
@@ -8,7 +8,7 @@ import (
 
 // StripComments function to remove comments from queries
 func StripComments(input string) string {
-	out := regexp.MustCompile(`(--|//)(.*)`).ReplaceAllLiteralString(input, "")
+	out := regexp.MustCompile(`--(.*)`).ReplaceAllLiteralString(input, "")
 	out = regexp.MustCompile("(?m)^\\s*$[\r\n]*").ReplaceAllLiteralString(out, "")
 	return strings.Trim(out, "\r\n")
 }
@@ -29,10 +29,14 @@ func FormatQuery(sqlStatement string, parameters map[string]interface{}) string 
 // SplitQueries function to split SQL statements into array of queries
 func SplitQueries(sqlStatement string) []string {
 	var output []string
-	queries := strings.Split(sqlStatement, ";")
-	for _, query := range queries[:len(queries)-1] {
-		queryOut := strings.TrimSpace(query) + ";"
-		output = append(output, queryOut)
+	queries := strings.Split(sqlStatement, ";\n")
+	for _, query := range queries {
+		query := strings.TrimSpace(query)
+		isMatched, _ := regexp.MatchString(";$", query)
+		if !isMatched {
+			query = fmt.Sprintf("%s;", query)
+		}
+		output = append(output, query)
 	}
 	return output
 }
